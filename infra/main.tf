d3d1f8fdb506f2806bd98b361257ba7be3ee8c22
magnus <pedmagnus@gmail.com>
2024-01-14 17:56:37 -0300

    chore: creating infra as code

---
+++ infra/main.tf
@@ -0,0 +1,63 @@
+provider "aws" {
+
+  access_key = "mock_access_key"
+  secret_key = "mock_secret_key"
+  region     = "sa-east-1"
+  s3_use_path_style           = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    s3             = "http://localstack:4566"
+  }
+
+}
+
+
+resource "aws_s3_bucket" "projects_bucket" {
+  bucket = "projects"
+}
+
+/* resource "aws_elasticache_cluster" "projects_redis" { */
+/*   cluster_id           = "projectsredis" */
+/*   engine               = "redis" */
+/*   node_type            = "t4g.micro" */
+/*   num_cache_nodes      = 1 */
+/*   parameter_group_name = "default.redis3.2" */
+/*   engine_version       = "3.2.10" */
+/*   port                 = 6379 */
+/* } */
+
+resource "aws_sqs_queue" "detection_service" {
+  name                      = "detect-pattern"
+  delay_seconds             = 0
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.detection_service_deadletter.arn
+    maxReceiveCount     = 4
+  })
+}
+
+resource "aws_sqs_queue" "detection_service_deadletter" {
+  name = "detect-pattern-deadletter"
+}
+
+
+resource "aws_sqs_queue" "metrics_service" {
+  name                      = "measure-pattern"
+  delay_seconds             = 0
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.metrics_service_deadletter.arn
+    maxReceiveCount     = 4
+  })
+}
+
+resource "aws_sqs_queue" "metrics_service_deadletter" {
+  name = "measure-pattern-deadletter"
+}
