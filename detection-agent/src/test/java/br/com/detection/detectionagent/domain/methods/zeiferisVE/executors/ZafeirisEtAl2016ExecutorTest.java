1489802a99a87518ab4882a58faf480b3725453b
magnus <pedmagnus@gmail.com>
2024-05-07 19:05:04 -0300

    test: creating test for ZafeirisEtAl2016Executor

---
+++ detection-agent/src/test/java/br/com/detection/detectionagent/domain/methods/zeiferisVE/executors/ZafeirisEtAl2016ExecutorTest.java
@@ -0,0 +1,53 @@
+package br.com.detection.detectionagent.domain.methods.zeiferisVE.executors;
+
+import br.com.detection.detectionagent.domain.methods.zeiferisVE.ZafeirisEtAl2016Candidate;
+import fixtures.Zafeiris;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ExtendWith(MockitoExtension.class)
+class ZafeirisEtAl2016ExecutorTest {
+
+    private ZafeirisEtAl2016Executor zafeirisEtAl2016Executor;
+
+    @BeforeEach
+    void setUp() {
+        this.zafeirisEtAl2016Executor = new ZafeirisEtAl2016Executor();
+    }
+
+    @Test
+    @DisplayName("Should test refactor for both parameter null")
+    public void shouldTestRefactorForBothParameterNull() {
+        var result = assertThrows(IllegalArgumentException.class,
+                () -> zafeirisEtAl2016Executor.refactor(null, null));
+
+        assertEquals("Candidate cannot be null", result.getMessage());
+    }
+
+    @Test
+    @DisplayName("Should test refactor for java files null")
+    public void shouldTestRefactorForJavaFilesNull() {
+        var result = assertThrows(IllegalArgumentException.class,
+                () -> zafeirisEtAl2016Executor.refactor(ZafeirisEtAl2016Candidate.builder().build(), null));
+
+        assertEquals("JavaFiles cannot be null", result.getMessage());
+    }
+
+    @Test
+    @DisplayName("Should test refactor for candidate")
+    public void shouldTestRefactorForCandidate() {
+        var candidate = Zafeiris.getCandidate();
+        var javaFiles = Zafeiris.createJavaFiles();
+
+        zafeirisEtAl2016Executor.refactor(candidate, javaFiles);
+
+        assertEquals(Zafeiris.REFACTORED_PARENT, javaFiles.getFirst().getCompilationUnit().toString());
+        assertEquals(Zafeiris.REFACTORED_CHILD, candidate.getCompilationUnit().toString());
+    }
+}
--- detection-agent/src/test/java/fixtures/Zafeiris.java
+++ detection-agent/src/test/java/fixtures/Zafeiris.java
@@ -1,7 +1,10 @@
 package fixtures;
 
+import br.com.detection.detectionagent.domain.dataExtractions.ast.AstHandler;
+import br.com.detection.detectionagent.domain.methods.zeiferisVE.ZafeirisEtAl2016Candidate;
 import br.com.detection.detectionagent.file.JavaFile;
 import br.com.detection.detectionagent.methods.dataExtractions.AbstractSyntaxTree;
+import com.github.javaparser.ast.CompilationUnit;
 
 import java.util.List;
 
@@ -9,9 +12,9 @@ public class Zafeiris {
 
     private static final AbstractSyntaxTree abstractSyntaxTree = new AbstractSyntaxTree();
 
-    private static final String child = """
+    public static final String CHILD = """
             package jade.imtp.leap.JICP;
-            
+                        
             public class JICPSPeer extends JICPPeer {
             	protected static Logger myLogger = Logger.getMyLogger( JICPSPeer.class.getName() );
             	private SSLContext ctx = null;
@@ -35,9 +38,9 @@ public class Zafeiris {
 
             } // end class""";
 
-    private static final String parent = """
+    public static final String PARENT = """
             package jade.imtp.leap.JICP;
-            
+                        
             public class JICPPeer {
             	private static final int POOL_SIZE = 50;
 
@@ -74,20 +77,135 @@ public class Zafeiris {
 
             		return localTA;
             	}
-            }""";
+            }
+""";
+
+    public final static String REFACTORED_CHILD = """
+package jade.imtp.leap.JICP;
+
+public class JICPSPeer extends JICPPeer {
+
+    protected static Logger myLogger = Logger.getMyLogger(JICPSPeer.class.getName());
+
+    private SSLContext ctx = null;
+
+    protected void beforeActivate(ICP.Listener l, String peerID, Profile p) throws ICPException {
+        if (myLogger.isLoggable(Logger.FINE)) {
+            myLogger.log(Logger.FINE, "About to activate JICP peer.");
+        }
+        // create context at activation time
+        ctx = SSLHelper.createContext();
+        setUseSSLAuth(SSLHelper.needAuth());
+        if (myLogger.isLoggable(Logger.FINE)) {
+            myLogger.log(Logger.FINE, "activate() context created ctx=" + ctx);
+        }
+    }
+
+    protected TransportAddress afterActivate(ICP.Listener l, String peerID, Profile p, TransportAddress ta) throws ICPException {
+        if (myLogger.isLoggable(Logger.INFO)) {
+            myLogger.log(Logger.INFO, "JICP Secure Peer activated. (auth=" + getUseSSLAuth() + ", ta=" + ta + ")");
+        }
+        return ta;
+    }
+}
+// end class
+""";
+
+    public final static String REFACTORED_PARENT = """
+package jade.imtp.leap.JICP;
+
+public class JICPPeer {
+
+    private static final int POOL_SIZE = 50;
+
+    private JICPClient client;
+
+    private JICPServer server;
+
+    private Ticker ticker;
+
+    private String myID;
+
+    private int connectionTimeout = 0;
+
+    public static final String CONNECTION_TIMEOUT = "jade_imtp_leap_JICP_JICPPeer_connectiontimeout";
+
+    /**
+     * Start listening for internal platform messages on the specified port
+     */
+    public final TransportAddress activate(ICP.Listener l, String peerID, Profile p) throws ICPException {
+        beforeActivate(l, peerID, p);
+        TransportAddress superReturnVar = doActivate(l, peerID, p);
+        return afterActivate(l, peerID, p, superReturnVar);
+    }
+
+    private TransportAddress doActivate(ICP.Listener l, String peerID, Profile p) throws ICPException {
+        myID = peerID;
+        connectionTimeout = Integer.parseInt(p.getParameter(CONNECTION_TIMEOUT, "0"));
+        // Start the client
+        client = new JICPClient(getProtocol(), getConnectionFactory(), POOL_SIZE);
+        // Start the server listening for connections
+        server = new JICPServer(p, this, l, getConnectionFactory(), POOL_SIZE);
+        server.start();
+        // Start the Ticker
+        ticker = new Ticker(60000);
+        ticker.start();
+        // Creates the local transport address
+        TransportAddress localTA = getProtocol().buildAddress(server.getLocalHost(), String.valueOf(server.getLocalPort()), null, null);
+        return localTA;
+    }
+
+    protected void beforeActivate(ICP.Listener l, String peerID, Profile p) throws ICPException {
+    }
+
+    protected TransportAddress afterActivate(ICP.Listener l, String peerID, Profile p, TransportAddress ta) throws ICPException {
+        return ta;
+    }
+}
+""";
+
+    private static CompilationUnit getParentCu() {
+        return (CompilationUnit) AbstractSyntaxTree.parseSingle(PARENT);
+    }
+
+    private static CompilationUnit getChildCu() {
+        return (CompilationUnit) AbstractSyntaxTree.parseSingle(CHILD);
+    }
 
     public static List<JavaFile> createJavaFiles() {
         var javaFile1 = JavaFile.builder()
                 .name("JICPPeer")
-                .originalClass(parent)
+                .originalClass(PARENT)
+                .parsed(getParentCu())
                 .build();
         var javaFile2 = JavaFile.builder()
                 .name("JICPSPeer")
-                .originalClass(child)
+                .originalClass(CHILD)
+                .parsed(getChildCu())
                 .build();
-        var javaFiles = List.of(javaFile1, javaFile2);
 
-        abstractSyntaxTree.parseAll(javaFiles);
-        return javaFiles;
+        return List.of(javaFile1, javaFile2);
+    }
+
+    public static ZafeirisEtAl2016Candidate getCandidate() {
+        var cu = (CompilationUnit) AbstractSyntaxTree.parseSingle(CHILD);
+        var clazz = AstHandler.getClassOrInterfaceDeclaration(cu).orElse(null);
+        var overriddenMethod = AstHandler.getMethods(cu).getFirst();
+        var overridingMethod = AstHandler.getMethods((CompilationUnit) AbstractSyntaxTree.parseSingle(PARENT)).getFirst();
+        var superCall = AstHandler.getSuperCalls(overriddenMethod).getFirst();
+        var file = JavaFile.builder()
+                .name("JICPSPeer")
+                .originalClass(CHILD)
+                .parsed(cu)
+                .build();
+
+        return ZafeirisEtAl2016Candidate.builder()
+                .file(file)
+                .compilationUnit(file.getCompilationUnit())
+                .classDcl(clazz)
+                .overriddenMethod(overriddenMethod)
+                .overridingMethod(overridingMethod)
+                .superCall(superCall)
+                .build();
     }
 }
