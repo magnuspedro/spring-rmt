2ba3297c5bf453b52beadf3eef3e4d9ac1413af3
magnus <pedmagnus@gmail.com>
2024-05-08 17:27:16 -0300

    test: creating test for wei factory verifier

---
+++ detection-agent/src/test/java/fixtures/Wei.java
@@ -0,0 +1,87 @@
+package fixtures;
+
+import br.com.detection.detectionagent.file.JavaFile;
+import br.com.detection.detectionagent.methods.dataExtractions.AbstractSyntaxTree;
+
+import java.util.List;
+
+public class Wei {
+
+    private static final String FACTORY_INTERFACE = """
+            package factory;
+
+            public interface Logger{
+              void writeLog();
+            }
+                       """;
+    private static final String FACTORY_DATABASE_LOGGER_IMPLEMENTATION = """
+            package factory;
+
+            public class DataBaseLogger implements Logger {
+              
+              public void writeLog(String message) {
+                System.out.println("Logging to database: " + message);
+              }
+            }
+            """;
+    private static final String FACTORY_LOGGER_IMPLEMENTATION = """
+            package factory;
+
+            public class FileLogger implements Logger {
+              
+              public void writeLog(String message) {
+                System.out.println("Logging to file: " + message);
+              }
+            }
+            """;
+
+    private static final String FACTORY = """
+            package factory;
+
+            public class LoggerFactory {
+              
+              public Logger createLogger(char type){
+                if(type == 'D'){
+                  Logger logger = new DataBaseLogger();
+                  return logger;
+                }
+                else if(type == 'F'){
+                  Logger logger = new FileLogger();
+                  return logger;
+                }
+                else{
+                  return null;
+                }
+
+              }
+            }
+            """;
+
+    public static List<JavaFile> createJavaFiles() {
+        return List.of(
+                JavaFile.builder()
+                        .name("Logger.java")
+                        .path("factory/")
+                        .originalClass(FACTORY_INTERFACE)
+                        .parsed(AbstractSyntaxTree.parseSingle(FACTORY_INTERFACE))
+                        .build(),
+                JavaFile.builder()
+                        .name("FileLogger.java")
+                        .path("factory/")
+                        .originalClass(FACTORY_LOGGER_IMPLEMENTATION)
+                        .parsed(AbstractSyntaxTree.parseSingle(FACTORY_LOGGER_IMPLEMENTATION))
+                        .build(),
+                JavaFile.builder()
+                        .name("DataBaseLogger.java")
+                        .path("factory/")
+                        .originalClass(FACTORY_DATABASE_LOGGER_IMPLEMENTATION)
+                        .parsed(AbstractSyntaxTree.parseSingle(FACTORY_DATABASE_LOGGER_IMPLEMENTATION))
+                        .build(),
+                JavaFile.builder()
+                        .name("LoggerFactory.java")
+                        .path("factory/")
+                        .originalClass(FACTORY)
+                        .parsed(AbstractSyntaxTree.parseSingle(FACTORY))
+                        .build());
+    }
+}
