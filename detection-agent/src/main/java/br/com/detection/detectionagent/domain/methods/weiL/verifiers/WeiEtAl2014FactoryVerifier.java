0128fc65c150ddd7afb96472b7321472a66b1e79
magnus <pedmagnus@gmail.com>
2024-05-08 17:26:51 -0300

    refact: making factory verifier more concise

--- detection-agent/src/main/java/br/com/detection/detectionagent/domain/methods/weiL/verifiers/WeiEtAl2014FactoryVerifier.java
+++ detection-agent/src/main/java/br/com/detection/detectionagent/domain/methods/weiL/verifiers/WeiEtAl2014FactoryVerifier.java
@@ -4,12 +4,12 @@ import br.com.detection.detectionagent.domain.dataExtractions.ast.AstHandler;
 import br.com.detection.detectionagent.domain.methods.weiL.WeiEtAl2014Candidate;
 import br.com.detection.detectionagent.domain.methods.weiL.WeiEtAl2014FactoryCandidate;
 import br.com.detection.detectionagent.file.JavaFile;
+import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.body.Parameter;
 import com.github.javaparser.ast.expr.BinaryExpr;
-import com.github.javaparser.ast.expr.NameExpr;
 import com.github.javaparser.ast.expr.ObjectCreationExpr;
-import com.github.javaparser.ast.expr.SimpleName;
 import com.github.javaparser.ast.nodeTypes.NodeWithSimpleName;
 import com.github.javaparser.ast.stmt.IfStmt;
 import com.github.javaparser.ast.type.ClassOrInterfaceType;
@@ -24,108 +24,72 @@ public class WeiEtAl2014FactoryVerifier extends WeiEtAl2014Verifier {
 
     protected boolean areIfStmtsValid(List<JavaFile> javaFiles, JavaFile file, MethodDeclaration method, Collection<IfStmt> ifStatements) {
         final var baseType = AstHandler.getMethodReturnClassType(method);
-        if (baseType.isEmpty()) {
-            return false;
-        }
 
-        final var parameter = method.getParameters()
-                .stream()
-                .findFirst()
-                .orElseThrow(IllegalArgumentException::new);
+        return baseType.filter(classOrInterfaceType -> !ifStatements.isEmpty()
+                && ifStatements.stream()
+                .allMatch(ifStmt -> ifStmtIsValid(javaFiles, classOrInterfaceType, method.getParameters(), ifStmt)))
+                .isPresent();
 
-        return !ifStatements.isEmpty()
-                && ifStatements.stream().allMatch(ifStmt -> ifStmtIsValid(javaFiles, baseType.get(), parameter, ifStmt));
     }
 
-    private boolean ifStmtIsValid(List<JavaFile> dataHandler, ClassOrInterfaceType baseType, Parameter parameter, IfStmt ifStmt) {
-
+    private boolean ifStmtIsValid(List<JavaFile> javaFiles, ClassOrInterfaceType baseType, List<Parameter> parameter, IfStmt ifStmt) {
         final var binaryExpr = ifStmt.getChildNodes().stream()
                 .filter(BinaryExpr.class::isInstance)
                 .map(BinaryExpr.class::cast)
                 .findFirst();
 
         final var objectCreationExpr = AstHandler.getObjectCreationExprList(ifStmt);
+        final var hasReturn = AstHandler.getReturnStmt(ifStmt).isPresent();
+        final var hasValidReturn = this.hasReturnTypeAndHasValidSubtype(javaFiles, baseType, objectCreationExpr);
 
-        final boolean parameterIsUsed = (binaryExpr.isPresent() || !objectCreationExpr.isEmpty())
-                && objectCreationExpr.stream().anyMatch(it -> isParameterUsedInIfStmtConditional(parameter, binaryExpr, it));
-
-        final boolean hasValidReturn = this.hasReturnTypeAndHasValidSubtype(dataHandler, baseType, ifStmt);
-
-        return parameterIsUsed && hasValidReturn;
+        return ((binaryExpr.isPresent()
+                || !objectCreationExpr.isEmpty())
+                && isParameterUsedInIfStmtConditional(parameter, binaryExpr))
+                && hasReturn
+                && hasValidReturn;
     }
 
-    private boolean hasReturnTypeAndHasValidSubtype(List<JavaFile> javaFiles, ClassOrInterfaceType baseType, IfStmt ifStmt) {
-
-        final var returnStmt = AstHandler.getReturnStmt(ifStmt);
-        if (returnStmt.isPresent()) {
-
-            final var node = returnStmt.get().getChildNodes().stream().findFirst();
-            if (node.filter(NameExpr.class::isInstance).isPresent()) {
-                final var returnName = node.map(NameExpr.class::cast).get().getNameAsString();
-                final var varDclr = AstHandler.getVariableDeclarationInNode(ifStmt.getThenStmt(), returnName);
-                final var objectCreationExpr = varDclr.map(AstHandler::getObjectCreationExpr)
-                        .filter(Optional::isPresent)
-                        .map(Optional::get);
-
-                return this.isOfTypeOrIsSubtype(javaFiles, baseType, objectCreationExpr);
-            } else if (node.filter(ObjectCreationExpr.class::isInstance).isPresent()) {
-
-                final var objCreationExpr = node.map(ObjectCreationExpr.class::cast);
-                return this.isOfTypeOrIsSubtype(javaFiles, baseType, objCreationExpr);
-            }
-        }
-        return false;
+    private boolean hasReturnTypeAndHasValidSubtype(List<JavaFile> javaFiles, ClassOrInterfaceType baseType, List<ObjectCreationExpr> objectCreationExpr) {
+        return this.isOfTypeOrIsSubtype(javaFiles, baseType, objectCreationExpr);
     }
 
-    private boolean isOfTypeOrIsSubtype(List<JavaFile> javaFiles, ClassOrInterfaceType type,
-                                        Optional<ObjectCreationExpr> objCreationExpr) {
-
+    private boolean isOfTypeOrIsSubtype(List<JavaFile> javaFiles, ClassOrInterfaceType type, List<ObjectCreationExpr> objCreationExpr) {
         if (objCreationExpr.isEmpty()) {
             return false;
         }
 
-        final var classOrInterfaceType = objCreationExpr.map(ObjectCreationExpr::getType);
-        if (classOrInterfaceType.isEmpty()) {
-            return false;
-        }
-
-        final var declaration = javaFiles.stream()
-                .filter(f -> f.getFileNameWithoutExtension().equals(classOrInterfaceType.map(NodeWithSimpleName::getNameAsString).orElse("")))
+        final var objCreationExprTypes = objCreationExpr.stream()
+                .map(ObjectCreationExpr::getType)
+                .map(NodeWithSimpleName::getNameAsString)
+                .toList();
+        final var parent = javaFiles.stream()
+                .filter(f -> objCreationExprTypes.contains(f.getFileNameWithoutExtension()))
                 .map(JavaFile::getCompilationUnit)
                 .map(AstHandler::getClassOrInterfaceDeclaration)
                 .flatMap(Optional::stream)
-                .findFirst();
+                .toList();
+        final var isExtendedType = parent.stream()
+                .map(ClassOrInterfaceDeclaration::getExtendedTypes)
+                .flatMap(NodeList::stream)
+                .anyMatch(t -> t.equals(type));
+        final var isImplementedType = parent.stream()
+                .map(ClassOrInterfaceDeclaration::getImplementedTypes)
+                .flatMap(NodeList::stream)
+                .anyMatch(t -> t.equals(type));
 
-
-        return declaration.isPresent() && (declaration.get().getExtendedTypes().stream().anyMatch(t -> t.equals(type))
-                || declaration.get().getImplementedTypes().stream().anyMatch(t -> t.equals(type)));
+        return !parent.isEmpty() && (isExtendedType || isImplementedType);
     }
 
-    private boolean isParameterUsedInIfStmtConditional(Parameter parameter, Optional<BinaryExpr> binaryExpr,
-                                                       ObjectCreationExpr methodCallExpr) {
-
-        if (binaryExpr.isPresent()) {
-            final String name2 = AstHandler.getNameExpr(binaryExpr.get()).map(NodeWithSimpleName::getNameAsString).orElse("");
-
-            return parameter.getNameAsString().equals(name2)
-                    && BinaryExpr.Operator.EQUALS.equals(binaryExpr.get().getOperator());
-        } else if (methodCallExpr != null) {
-
-            final var hasParam = methodCallExpr.getChildNodes()
-                    .stream()
-                    .filter(NameExpr.class::isInstance)
-                    .map(NameExpr.class::cast)
-                    .anyMatch(n -> n.getNameAsString().equals(parameter.getNameAsString()));
-            final var isAnEqualsMethod = methodCallExpr.getChildNodes()
-                    .stream()
-                    .filter(SimpleName.class::isInstance)
-                    .map(SimpleName.class::cast)
-                    .anyMatch(n -> n.asString().equals(parameter.getNameAsString()));
-
-            return hasParam && isAnEqualsMethod;
+    private boolean isParameterUsedInIfStmtConditional(List<Parameter> parameter, Optional<BinaryExpr> binaryExpr) {
+        if (binaryExpr.isEmpty()) {
+            return false;
         }
+        final var name = AstHandler.getNameExpr(binaryExpr.get())
+                .map(NodeWithSimpleName::getNameAsString)
+                .orElse("");
 
-        return false;
+        return parameter.stream().anyMatch(param -> param.getNameAsString().equals(name))
+                && BinaryExpr.Operator.EQUALS.equals(binaryExpr.get().getOperator());
     }
 
     @Override
