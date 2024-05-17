f8a1abae49c78a15f76b8fc71fd28be997d78485
magnus <pedmagnus@gmail.com>
2024-05-17 02:53:40 -0300

    test: adding test for ExtractProjects

--- metrics-agent/src/main/java/br/com/metrics/metricsagent/extraction/ExtractProjects.java
+++ metrics-agent/src/main/java/br/com/metrics/metricsagent/extraction/ExtractProjects.java
@@ -5,6 +5,7 @@ import br.com.magnus.config.starter.file.extractor.FileExtractor;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -14,12 +15,13 @@ import java.nio.file.Path;
 @Component
 @RequiredArgsConstructor
 public class ExtractProjects {
-
-
     private final FileExtractor fileExtractor;
 
     @SneakyThrows
     public Path extractProject(String id, String bucket) {
+        Assert.notNull(id, "Id cannot be null");
+        Assert.notNull(bucket, "Bucket cannot be null");
+
         var files = fileExtractor.extract(bucket, id);
         var basePath = Files.createTempDirectory(id).toAbsolutePath();
         files.forEach(file -> createTempFile(basePath.toString(), file));
@@ -27,9 +29,11 @@ public class ExtractProjects {
         return basePath;
     }
 
-
     @SneakyThrows
     private void createTempFile(String basePath, JavaFile javaFile) {
+        Assert.notNull(javaFile.getName(), "Name cannot be null");
+        Assert.notNull(javaFile.getOriginalClass(), "Original class cannot be null");
+
         var file = new File(basePath + "/" + javaFile.getFullName());
         file.getParentFile().mkdirs();
         file.createNewFile();
